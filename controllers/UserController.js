af7f6809b6c8579481f7388fd277527a03010fbf
b-rachman <bobbyrachman97@outlook.com>
2020-10-14 16:57:12 +0700

    method to get,put and delete u_name & password data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -29,6 +29,67 @@ userRouter.post('/add',async (req,res)=>{
     }
 })
 
+userRouter.get('/data', async (req,res)=>{
+    const data = await User.find({})
+    if(data){
+        res.json(data)
+    } else {
+        res.status(404).json({
+            message:'Data not found'
+        })
+    }
+})
+
+userRouter.put('/update/:id', async (req,res)=>{
+    const {
+        username,
+        password
+    }=req.body
+    var saltRounds=10
+    const hashedPw = await bcrypt.hash(password, saltRounds)
+    const update= await User.findById(req.params.id)
+    if (update){
+        update.username=username,
+        update.password=hashedPw
+
+        const updateData= await update.save()
+        res.json('Update Success')
+        //res.json(updateData)
+    } else {
+        res.status(404).json({
+            message :'User not found'
+        })
+    }
+})
+
+userRouter.delete('/delete/:id', async(req,res)=>{
+    const data= await User.findById(req.params.id)
+    if(data && data!==0){
+        await data.remove()
+        res.json({
+            message:'data removed'
+        })
+    } else {
+        res.status(404).json({
+            message:'data not found'
+        })
+    }
+})
+
+userRouter.delete('/delete', async(req,res)=>{
+    const data= await User
+    if(data && data!==0){
+        await data.remove()
+        res.json({
+            message:'data removed'
+        })
+    } else {
+        res.status(404).json({
+            message:'data not found'
+        })
+    }
+})
+
 //login
 userRouter.post('/login', async (req, res) => {
     try{
